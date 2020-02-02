75606b2569898bec7bff2e1e68e9ec53dece9084
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-02-02 03:48:06 -0800

    Bulding HTML&CSS

---
+++ jb.js
@@ -0,0 +1,14 @@
+function menu() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu_bg").classList.toggle("change_bg")
+}
+
+function sign_up() {
+    document.getElementById("sign").classList.toggle("change_sign");
+}
+
+function login() {
+    document.getElementById("login").classList.toggle("change_login");
+}
+
