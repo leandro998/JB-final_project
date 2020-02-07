17df26cb93d9ce32cb9b82a45990df3ade908415
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-02-07 08:35:56 -0800

    version for apresentation

--- js/sort.js
+++ js/sort.js
@@ -68,7 +68,7 @@ const mergeSort = (list) =>{
     console.log(sortedArray);
   }
   
-// 
+
 
 //###################//
 //####QUICK SORT####//
@@ -128,32 +128,31 @@ function callQuickSort () {
 //###################//
 
 function Insertion_Sort(arr) {
-    for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] < arr[0]) {
-//move current element to the first position
-        arr.unshift(arr.splice(i, 1)[0]);
+      //move current element to the first position
+      arr.unshift(arr.splice(i, 1)[0]);
+    } else if (arr[i] > arr[i - 1]) {
+      continue; //leave current element where it is
+    } else {
+      //find where element should go
+      for (let j = 1; j < i; j++) {
+        if (arr[i] > arr[j - 1] && arr[i] < arr[j]) {
+          //move element
+          arr.splice(j, 0, arr.splice(i, 1)[0]);
+        }
+      }
     }
-    else if (arr[i] > arr[i - 1]) {
-        continue; //leave current element where it is
-    } 
-    else {
-        //find where element should go
-        for (let j = 1; j < i; j++) {
-            if (arr[i] > arr[j - 1] && arr[i] < arr[j]) {
-                //move element
-                    arr.splice(j, 0, arr.splice(i, 1)[0]);
-            }
-        }   
-    }
-    return arr;
+  }
+  return arr;
 }
-
 function callInsertionSort() {
-    let user_input = document.getElementById("is_array_input").value;
-    let array_input = user_input.split(",").map(elem => parseInt(elem, 10));
-    let sortedArray = Insertion_Sort(array_input);
-    document.getElementById("is_output").innerHTML = "The sorted array is: " + sortedArray;
-    console.log(sortedArray);
+  let user_input = document.getElementById("is_array_input").value;
+  let array_input = user_input.split(",").map(elem => parseInt(elem, 10));
+  let sortedArray = Insertion_Sort(array_input);
+  document.getElementById("is_output").innerHTML =
+    "Result: " + sortedArray;
+  console.log(sortedArray);
 }
 
 //###################//
@@ -161,36 +160,36 @@ function callInsertionSort() {
 //###################//
 
 function Selection_Sort(arr, compare_Function) {
-
-    function compare(a, b) {
-        return a - b;
-    } 
-        let min = 0;
-        let index = 0;
-        let temp = 0;
-    //{Function} compare_Function Compare function
-        compare_Function = compare_Function || compare;
-        for (let i = 0; i < arr.length; i += 1) {
-        index = i;
-        min = arr[i];
-        for (let j = i + 1; j < arr.length; j += 1) {
-            if (compare_Function(min, arr[j]) > 0) {
-            min = arr[j];
-            index = j;
+  function compare(a, b) {
+    return a - b;
+  }
+  let min = 0;
+  let index = 0;
+  let temp = 0;
+  //{Function} compare_Function Compare function
+  compare_Function = compare_Function || compare;
+  for (let i = 0; i < arr.length; i += 1) {
+    index = i;
+    min = arr[i];
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (compare_Function(min, arr[j]) > 0) {
+        min = arr[j];
+        index = j;
+      }
     }
-    }
-        temp = arr[i];
-        arr[i] = min;
-        arr[index] = temp;
-    }
-    //return sorted arr
-        return arr;
+    temp = arr[i];
+    arr[i] = min;
+    arr[index] = temp;
+  }
+  //return sorted arr
+  return arr;
 }
 
 function callSelectionSort() {
-    let user_input = document.getElementById("sl_input").value;
-    let array_input = user_input.split(",").map(elem => parseInt(elem, 10));
-    let sortedArray = Selection_Sort(array_input);
-    document.getElementById("sl_output").innerHTML = "The sorted array is: " + sortedArray;
-    console.log(sortedArray);
+  let user_input = document.getElementById("sl_input").value;
+  let array_input = user_input.split(",").map(elem => parseInt(elem, 10));
+  let sortedArray = Selection_Sort(array_input);
+  document.getElementById("sl_output").innerHTML =
+    "The sorted array is: " + sortedArray;
+  console.log(sortedArray);
 }
